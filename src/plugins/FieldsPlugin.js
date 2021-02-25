e82362eeb6b942ef8aa3a5d313b59d0674447318
Frank Fava <frank.fava@igomoon.com>
2021-02-25 19:34:28 +0100

    WIP

--- src/plugins/FieldsPlugin.js
+++ src/plugins/FieldsPlugin.js
@@ -6,8 +6,8 @@ const FieldTransformer = require('../fields/FieldTransformer');
 
 const clearFieldsJson = async compilation => {
     return new Promise(async (resolve, reject) => {
-        let distFolder = compilation.options.output.path;
-        glob(distFolder + '/**/fields.json', {}, (err, files) => {
+        let srcFolder = compilation.options.output.path;
+        glob(srcFolder + '/**/fields.json', {}, (err, files) => {
             files.forEach(file => {
                 fs.unlinkSync(file);
             });
@@ -27,42 +27,47 @@ class FieldsPlugin {
         compiler.hooks.watchRun.tapPromise('FieldsPlugin', clearFieldsJson);
 
         // Transform the fields.json
-        compiler.hooks.afterEmit.tapPromise('FieldsPlugin', async compilation => {
+        compiler.hooks.beforeRun.tapPromise('FieldsPlugin', async compilation => {
             return new Promise(async (resolve, reject) => {
-                // Set the distfolder to look for files
+                // Set the distFolder to look for files
                 // options.context gives root of the project.
 
-                let distFolder = compilation.options.output.path;
+				let srcFolder = `${compilation.options.context}/src`;
+				let distFolder = compilation.options.output.path;
 
                 // Handle fields.js file
                 await new Promise((resolve, reject) => {
                     // Find every modules fields.js file.
-                    glob(distFolder + '/**/fields.js', {}, (err, files) => {
-                        files.forEach(file => {
+                    glob(srcFolder + '/**/fields.js', {}, (err, files) => {
+						files.forEach(file => {
+							
+							let srcFile = file;
+							let distFile = file.replace(srcFolder, distFolder);
+
                             try {
-                                let fields = require(file);
+                                let fields = require(srcFile);
 
-                                // Transform to Json and append to fields.json file
-                                FieldTransformer.transform(file + 'on', fields);
+								// Transform to Json and append to fields.json file
+                                FieldTransformer.transform(distFile + 'on1', fields);
 
-                                if (!compilation.assets[file.replace(distFolder + '/', '') + 'on']) {
-                                    compilation.assets[file.replace(distFolder + '/', '') + 'on'] = {
+                                if (!compilation.assets[distFile.replace(distFolder + '/', '') + 'on1']) {
+                                    compilation.assets[distFile.replace(distFolder + '/', '') + 'on1'] = {
                                         source: function () { return Buffer.from(file) },
                                         size: function () { return Buffer.byteLength(file) },
-                                        existsAt: file + 'on',
+                                        existsAt: file + 'on1',
                                         emitted: true
                                     }
                                 }
 
                                 // Remove field.js file from dist directory.
-                                fs.unlinkSync(file);
-                                delete require.cache[require.resolve(file)];
+                                //fs.unlinkSync(distFile);
+                                delete require.cache[require.resolve(srcFile)];
 
                                 // Remove fields.js from the compilation
-                                delete compilation.assets[file.replace(distFolder + '/', '')];
+                                delete compilation.assets[srcFile.replace(srcFolder + '/', '')];
                             } catch (e) {
-                                delete require.cache[require.resolve(file)];
-                                console.log("Could not transform: " + file + "\nError: " + e.message);
+                                delete require.cache[require.resolve(srcFile)];
+                                console.log("Could not transform: " + srcFile + "\nError: " + e.message);
                             }
                         });
                         resolve(); // resolve glob promise
