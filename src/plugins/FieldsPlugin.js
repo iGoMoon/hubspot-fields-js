41c259e52e5ce357da2ecf139e1b1a3c80ce2506
jazzyclimber <jazzyclimber@gmail.com>
2021-12-11 20:30:52 -0600

    update distpath to account for copyWebpackPlugin

--- src/plugins/FieldsPlugin.js
+++ src/plugins/FieldsPlugin.js
@@ -26,54 +26,81 @@ class FieldsPlugin {
     // This is to ensure that we dont end up with duplicate fields.
     compiler.hooks.run.tapPromise('FieldsPlugin', clearFieldsJson);
     compiler.hooks.watchRun.tapPromise('FieldsPlugin', clearFieldsJson);
+    compiler.hooks.emit.tapPromise('FieldsPlugin', clearFieldsJson);
 
     // Transform the fields.json
     compiler.hooks.afterEmit.tapPromise('FieldsPlugin', async compilation => {
       return new Promise(async (resolve, reject) => {
+
         // Set the srcFolder to look for files
         // options.context gives root of the project.
-
-        // let distFo = compilation.options.output.path; 
         let srcFolder = path.resolve("./src");
 
-        // console.log(distFo);
 
         // Handle fields.js file
         await new Promise((resolve, reject) => {
           // Find every modules fields.js file.
           glob(srcFolder + '/**/fields.js', {}, (err, files) => {
-            console.log("files: ", files)
+            // console.log("files: ", files)
             files.forEach(file => {
+              file = path.resolve(file);
               try {
                 let fields = require(file);
 
                 // Grab full dist Path
-                let fullDistPath = file.replace("/src/", '/dist/')
+                let outputPath = compilation.options.output.path;
+                let assets = compilation.assets;
+                let tempFile = file.split("/").slice(-2).join('/');
+                let distPath;
+
+                const keys = Object.keys(assets);
+
+                keys.forEach(key => {
+                  // Ensures that the module is actually the real module 
+                  // and not a subset of the original 
+                  // example = ../new-card.module/fields.js
+                  // key = ../card.module/fields.js
+                  // tempFile = card.module/fields.js
+                  // Will reslove true on both emaple and key
+                  // Adding a / will ensure that the whole module name is included
+                  key = "/" + key;
+                  if (key.includes("/" + tempFile) && !key.includes("/" + tempFile + 'on')) {
+                    distPath = key;
+                  }
+                })
+
+                let fullDistPath = path.resolve(outputPath + "/" + distPath);
 
                 // Transform to Json and append to fields.json file
                 FieldTransformer.transform(path.resolve(fullDistPath) + 'on', fields);
 
-                // Likely unneeded at this point
-                // setting emitted to false because hs autoupload plugin 
-                // looks for this to be true in order to upload
-                compilation.assets[file.replace(srcFolder + '/', '')].emitted = false;
 
-                // remove fields.js from assets.
-                delete compilation.assets[file.replace(srcFolder + '/', '')];
+                if (!compilation.emittedAssets[distPath]) {
+                  // Clean up distpath 
+                  // Working with original copilation object
+                  distPath = distPath.replace("/", "");
 
-                // remove fields.js from emittedAssets Set
-                compilation.emittedAssets.delete(file.replace(srcFolder + '/', ''))
+                  // setting emitted to false because hs autoupload plugin 
+                  // looks for this to be true in order to upload
+                  compilation.assets[distPath].emitted = false;
 
-                // add fields.json to emittedAssets set
-                // Functionally tricks AutoUplaoder into thinking that 
-                // This file was updated as part of the actual webpack process
-                compilation.emittedAssets.add(file.replace(srcFolder + '/', '') + "on")
+                  // remove fields.js from assets.
+                  delete compilation.assets[distPath];
 
-                // Remove field.js file from dist directory.
-                // remove fields.js from cache so it will reupload
-                // on future watch saves
-                fs.unlinkSync(fullDistPath);
-                delete require.cache[require.resolve(fullDistPath)];
+                  // remove fields.js from emittedAssets Set
+                  compilation.emittedAssets.delete(distPath)
+
+                  // add fields.json to emittedAssets set
+                  // Functionally tricks AutoUplaoder into thinking that 
+                  // This file was updated as part of the actual webpack process
+                  compilation.emittedAssets.add(distPath + "on")
+
+                  // Remove field.js file from dist directory.
+                  // remove fields.js from cache so it will reupload
+                  // on future watch saves
+                  fs.existsSync(fullDistPath) ? fs.unlinkSync(fullDistPath) : null;
+                  delete require.cache[require.resolve(file)]
+                }
 
               } catch (e) {
                 delete require.cache[require.resolve(file)];
